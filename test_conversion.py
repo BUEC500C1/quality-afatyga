392336c530096edb8a90c8ff561cc22bc23a72f4
Alex <afatyga@bu.edu>
2020-01-26 17:13:10 -0500

    attempting to add even more error checking and more tests

--- test_conversion.py
+++ test_conversion.py
@@ -2,9 +2,18 @@ import hw1
 
 def test_normal():
 	assert hw1.conversion(1) == "I"
+	assert hw1.conversion(3) == "III"
 	assert hw1.conversion(4) == "IV"
 	assert hw1.conversion(9) == "IX"
+	assert hw1.conversion(20) == "XX"
+	assert hw1.conversion(24) == "XXIV"
+	assert hw1.conversion(24) == "XXIV"
+	assert hw1.conversion(99) == "XCIX"
+
 
 def test_abnormal():
 	assert hw1.conversion(-1) == ""
 	assert hw1.conversion(0) == ""
+	assert hw1.conversion(-142) == ""
+	assert hw1.conversion(-13295) == ""
+	assert hw1.conversion("A") == ""
