606922dee4527b842ecada5e00794a2bfebff261
Alex <afatyga@bu.edu>
2020-01-26 16:55:56 -0500

    separating into 2 diff tests

--- test_conversion.py
+++ test_conversion.py
@@ -1,8 +1,10 @@
 import hw1
 
-def test_answer():
+def test_normal():
 	assert hw1.conversion(1) == "I"
 	assert hw1.conversion(4) == "IV"
 	assert hw1.conversion(9) == "IX"
+
+def test_abnormal():
 	assert hw1.conversion(-1) == ""
 	assert hw1.conversion(0) == ""
