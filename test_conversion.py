ddd07b7dde69d3354a0ff6718268f2ccf74a372f
Alex <afatyga@bu.edu>
2020-01-26 16:54:04 -0500

    adding in error checking slowly

--- test_conversion.py
+++ test_conversion.py
@@ -4,3 +4,5 @@ def test_answer():
 	assert hw1.conversion(1) == "I"
 	assert hw1.conversion(4) == "IV"
 	assert hw1.conversion(9) == "IX"
+	assert hw1.conversion(-1) == ""
+	assert hw1.conversion(0) == ""
